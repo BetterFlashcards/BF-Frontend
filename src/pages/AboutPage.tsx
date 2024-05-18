59f2eedb3a69c1b22fb441028fe12e5db6ac2992
duc <nguyenanhduc9a1@gmail.com>
2024-05-18 12:19:42 +0200

    Update visual

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,17 +1,19 @@
+import React from "react";
+import { FaBookOpen, FaLayerGroup, FaMapSigns } from "react-icons/fa";
+
 function AboutPage() {
   return (
-      <div className="about-page">
-          <h2>About Flashcards App</h2>
-          <p>The Flashcards App is a powerful tool for efficient learning. Here are some of its main features:</p>
-          <ul>
-              <li><strong>Interactive Flashcards:</strong> Learn and review topics using interactive flashcards.</li>
-              <li><strong>Custom Decks:</strong> Create your own decks of flashcards on any topic.</li>
-              <li><strong>Easy Navigation:</strong> Navigate through the app with a user-friendly interface.</li>
-          </ul>
-          <p>Start learning with Flashcards App today and take your knowledge to the next level!</p>
-      </div>
+    <div className="about-page">
+      <h2>About Flashcards App</h2>
+      <p>The Flashcards App is a powerful tool for efficient learning. Here are some of its main features:</p>
+      <ul>
+        <li><FaBookOpen className="icon" /><strong> Interactive Flashcards:</strong> Learn and review topics using interactive flashcards.</li>
+        <li><FaLayerGroup className="icon" /><strong> Custom Decks:</strong> Create your own decks of flashcards on any topic.</li>
+        <li><FaMapSigns className="icon" /><strong> Easy Navigation:</strong> Navigate through the app with a user-friendly interface.</li>
+      </ul>
+      <p>Start learning with Flashcards App today and take your knowledge to the next level!</p>
+    </div>
   );
 }
 
 export default AboutPage;
-  
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaArrowRight } from 'react-icons/fa';
 
 const Home: React.FC = () => {
   return (
     <div className="home-page">
       <h1>Welcome to Flashcards App!</h1>
       <p>This is a powerful tool for efficient learning. Start creating your flashcards now!</p>
-      <Link to="/decks" className="btn btn-primary">Get Started</Link>
+      <div className="btn-container">
+        <Link to="/decks" className="btn btn-primary">
+          Get Started <FaArrowRight className="icon" />
+        </Link>
+      </div>
+      <div className="animations">
+        <div className="circle one"></div>
+        <div className="circle two"></div>
+        <div className="circle three"></div>
+      </div>
     </div>
   );
 };
